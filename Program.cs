a82adfe0fb443268053bca2268938830f804ee72
berkaybayar <berkaybyr@icloud.com>
2022-04-26 14:54:29 +0300

    #5

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseMiddleware<IPControlMiddleware>();
+app.UseMiddleware<IPControlMiddleware>();
 
 //sapp.UseHttpsRedirection();
 
